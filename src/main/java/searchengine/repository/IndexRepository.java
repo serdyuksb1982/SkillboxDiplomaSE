c9734846cdaee13b854275e94055f8f552b726c6
Sergey Serdyuk <serdyuksb@gmail.com>
2023-01-23 17:46:32 +0300

    Refactor. correct code: Repositories

--- src/main/java/searchengine/repository/IndexRepository.java
+++ src/main/java/searchengine/repository/IndexRepository.java
@@ -1,18 +1,10 @@
 package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import searchengine.model.IndexModel;
-import searchengine.model.LemmaModel;
-import searchengine.model.PageModel;
-
-import java.util.List;
 
 @Repository
 public interface IndexRepository extends JpaRepository<IndexModel, Long> {
-    @Query(value = "SELECT i.* FROM Words_index i WHERE i.lemma_id IN :lemmas AND i.page_id IN :pages", nativeQuery = true)
-    List<IndexModel> findByPagesAndLemmas(@Param("lemmas") List<LemmaModel> lemmaListId,
-                                          @Param("pages") List<PageModel> pageListId);
+
 }
